77c116ccbbb2e4a850468fa1efabc84d744f9850
Miguel-Martinho <58664193+Miguel-Martinho@users.noreply.github.com>
2020-01-20 22:37:02 +0000

    Ap

--- ShuffleProject/card_naming.py
+++ ShuffleProject/card_naming.py
@@ -8,8 +8,9 @@ height = 160
 width = 110
 outline = 0
 
+
 class Card():
-    def __init__(self, color, x,y,width,height, outline, index):
+    def __init__(self, color, x,y,width,height, outline, index, flipped):
         self.color = color
         self.x = x
         self.y = y
@@ -17,67 +18,35 @@ class Card():
         self.height = height
         self.outline = outline
         self.index = 1
+        self.flipped = 0
+        self.ingame = True
+        self.text = " "
 
-    def draw(self,win,outline):
-        #Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, self.outline, (self.x,self.y,self.width+4,self.height+4),2)
-            
-        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),self.outline)
+    def draw(self, win, outline=None, flipped=None, ingame=None):
+        if self.ingame == True:
+            if self.flipped == 1:
+                pygame.draw.rect(win, (255, 0, 0), (self.x,self.y,self.width,self.height),outline)
+                if self.text != '':
+                    font = pygame.font.SysFont('NotoSans', 30)
+                    text = font.render(self.text, 1, (255,255,0))
+                    win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+            else:    
+                pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height))
+        
 
     def __repr__(self):
-        return f"{self.color} {self.index}"
+        return f"{self.index} {self.outline}"
 
     def isOver(self, pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
+        if self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height:
+            return True
             
         return False
 
-def get_deck():
+def get_deck(cardinx, cardiny):
     deck = []
-    for dx in range(0, 4):
-        for dy in range(0, 3):
-            deck.append(Card((0,200,100), x+width*dx+(dx*10), y+height*dy+(dy*10), width, height, outline, 0))
-    return deck 
-
-class Circle():
-    def __init__(self, color, x,y, radius, outline, index):
-        self.color = color
-        self.x = Card.x
-        self.y = Card.y
-        self.radius = radius
-        self.index = index
-
-    def draw(self,win):
-        #Call this method to draw the button on the screen
-        pygame.draw.circle(win, (100,100,100), 475, 160, 80)
-
-
-class Triangle():
-    def __init__(self, color, points, outline):
-        self.color = color
-        self.points = points()
-        self.outline = outline
-
-    def draw(self,win):
-        #Call this method to draw the button on the screen
-        pygame.draw.polygon(win, (200,200,0), ((475, 140), (435,210), (515, 210)))
-
-#dois arrays 1 com cartas e 1 com shapes, cartas.append(shapes)
-
-
-class Square():
-    def __init__(self, color, x,y,width,height, outline):
-        self.color = color
-        self.x = Card.x
-        self.y = Card.y
-        self.width = width
-        self.height = height
-        self.outline = outline
-
-    def draw(self,win,outline):
-        #Call this method to draw the button on the screen
-        pygame.draw.rect(win, (100,100,100), (400,100), (200,200), 0)
+    for dx in range(0, cardinx):
+        for dy in range(0, cardiny):
+            deck.append(Card((0,200,100), x+width*dx+(dx*10), y+height*dy+(dy*10), width, height, outline, 0, 0))
+    return deck
--- ShuffleProject/shuffle2.0.py
+++ ShuffleProject/shuffle2.0.py
@@ -11,8 +11,7 @@ green = (0,255,0)
 myimage = pygame.image.load("shuffle.png")
 imagerect = (210,-50)
 res = [1280, 720]
-deck = get_deck()
-deck_flipped = []
+deck = get_deck(0, 0)
 
 gameDisplay = pygame.display.set_mode((res))
 gameDisplay.blit(myimage, imagerect)
@@ -25,22 +24,21 @@ yellowbutton6x6 = button(yellow, 560, 410, 125, 30, '6x6')
 exitbutton = button(yellow, 560, 480, 125, 30, 'EXIT')
 exitbutton2 = button(yellow, 50, 660, 125, 30, 'EXIT')
 
+numberofcardsturned = 0
+car1turned = 0
+car2turned = 0
 
-def get_index():
-    index = [1,1,2,2,3,3,4,4,5,5,6,6]
-    random.shuffle(index)
-    for i in range(0, 12):
-            deck[i].index = (index[i])
-    return deck 
 
-'''
-def get_shape(deck):
-    compendium = []
-    for dx in range(0, 4):
-        for dy in range(0, 3):
-            compendium.append(Tirangle(random.randint(0,255),200,random.randint(0,255)), ((deck[dx].x/2, deck[dy].y/3), (deck[dx].x/3,deck[dy].y/3), ((deck[dx].x/3)*2,deck[dy].y/3))
+def get_index(x, y):
+    index = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18]
+    indextemp = index[:(x*y)]
+    random.shuffle(indextemp)
+    for i in range(0, x*y):
+        print(i)
+        deck[i].index = indextemp[i]
+        deck[i].text = str(indextemp[i])
     return deck
-'''
+
 def main_menu_draw():
     gameDisplay.blit(myimage, imagerect)
     yellowbutton4x3.draw (gameDisplay)
@@ -88,103 +86,145 @@ def mouse_over_check(pos):
         exitbutton.color = yellow
         main_menu_draw
             
-    exitbutton2.draw (gameDisplay, (0,0,0))
 
 pygame.display.set_caption('Shuffle')
 clock = pygame.time.Clock()
 crashed = False
 gamescreen = False
+timetowait = 0
 while not crashed:
+    if numberofcardsturned == 2:
+        timetowait = 1000
+    gameDisplay.fill(black)
 
     for event in pygame.event.get():
         pos = pygame.mouse.get_pos()
+        mouse_click = pygame.mouse.get_pressed()
         if event.type == pygame.QUIT:
             crashed = True
         
-        #MOUSE OVER MENU
-        if event.type == pygame.MOUSEMOTION and gamescreen == False:
-            mouse_over_check(pos)
-            main_menu_draw()
+    #MOUSE OVER MENU
+    if gamescreen == False:
+        mouse_over_check(pos)
+        main_menu_draw()
 
-        #MOUSE OVER IN-GAME
-        if gamescreen == True and event.type == pygame.MOUSEMOTION:
-            for i in range(0,len(deck)):
-                if deck[i].isOver(pos):
+    #MOUSE OVER IN-GAME
+    if gamescreen == True:
+        for i in range(0,len(deck)):
+            if deck[i].isOver(pos):
+                if deck[i].flipped == 0:
                     deck[i].color = (200,200,200)
                     for Card in deck:
-                        Card.draw(gameDisplay ,0)
+                        Card.draw(gameDisplay ,0 , deck[i].flipped)
+                elif deck[i].flipped == 1:
+                    deck[i].color = (200,200,200)
+                    for Card in deck:
+                        Card.draw(gameDisplay ,2 , deck[i].flipped)
+            else:
+                deck[i].color = (0,200,100)
+                for Card in deck:
+                    Card.draw(gameDisplay, 0)
+            exitbutton2.draw(gameDisplay)
+        
+    
+    #MOUSE CLICK
+    if mouse_click[0] == 1:
+        #MOUSE CLICK IN MENU
+        if gamescreen == False:
+            if yellowbutton4x3.isOver(pos):
+                gameDisplay.fill(black)
+                gamescreen = True
+                deck = get_deck(4, 3)
+                get_index(4, 3)
+                for Card in deck:
+                    Card.draw(gameDisplay)
+                exitbutton2.draw(gameDisplay)
+                
+
+            if gamescreen == False and yellowbutton4x4.isOver(pos):
+                gameDisplay.fill(black)
+                gamescreen = True
+                deck = get_deck(4, 4)
+                get_index(4, 4)
+                for Card in deck:
+                    Card.draw(gameDisplay, 0)
+                exitbutton2.draw(gameDisplay)
+                pygame.display.flip()
+                
+            
+            if gamescreen == False and yellowbutton5x4.isOver(pos):
+                gameDisplay.fill(black)
+                gamescreen = True
+                deck = get_deck(5, 4)
+                get_index(5, 4)
+                for Card in deck:
+                    Card.draw(gameDisplay, 0)
+                exitbutton2.draw(gameDisplay)
+                pygame.display.flip()
+            
+            if gamescreen == False and yellowbutton6x5.isOver(pos):
+                gameDisplay.fill(black)
+                gamescreen = True
+                deck = get_deck(6, 5)
+                get_index(6, 5)
+                for Card in deck:
+                    Card.draw(gameDisplay, 0)
+                exitbutton2.draw(gameDisplay)
+                pygame.display.flip()
+
+            if gamescreen == False and yellowbutton6x6.isOver(pos):
+                gameDisplay.fill(black)
+                gamescreen = True
+                deck = get_deck(6, 6)
+                get_index(6, 6)
+                for Card in deck:
+                    Card.draw(gameDisplay, 0)
+                exitbutton2.draw(gameDisplay)
+                pygame.display.flip()
+                
+
+            if gamescreen == False and exitbutton.isOver(pos):
+                quit()
+        #MOUSE CLICK IN-GAME
+        elif gamescreen == True:
+            i = 0
+            while i in range(0,len(deck)):
+                if deck[i].isOver(pos):
+                    deck[i].outline = 2
+                    deck[i].color == (0,200,100)
+                    if deck[i].flipped == 0:
+                        deck[i].flipped = 1
+                        if car1turned == 0:
+                            car1turned = i
+                            numberofcardsturned += 1
+                        else:
+                            car2turned = i
+                            numberofcardsturned += 1
+                    
+                        if numberofcardsturned == 2:
+                            if deck[car1turned].index == deck[car2turned].index:
+                                deck[car1turned].ingame = 0
+                                deck[car2turned].ingame = 0
+                                car1turned = 0
+                                car2turned = 0
+                            else:
+                                deck[car1turned].flipped = 0
+                                deck[car2turned].flipped = 0
+                                car1turned = 0
+                                car2turned = 0
+                                numberofcardsturned = 0
+
+
                 else:
                     deck[i].color = (0,200,100)
-                    for Card in deck:
-                        Card.draw(gameDisplay, 0)
-        #MOUSE CLICK
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            #MOUSE CLICK IN MENU
-            if gamescreen == False:
-                if yellowbutton4x3.isOver(pos):
-                    gameDisplay.fill(black)
-                    gamescreen = True
-                    get_deck()
-                    get_index()
-                    for Card in deck:
-                        Card.draw(gameDisplay, 0)
-                    exitbutton2.draw(gameDisplay)
-
-                if gamescreen == False and yellowbutton4x4.isOver(pos):
-                    gameDisplay.fill(black)
-                    gamescreen = True
-                    get_deck()
-                    for Card in deck:
-                        Card.draw(gameDisplay, 0)
-                    exitbutton2.draw(gameDisplay)
+                i += 1
+            if gamescreen == True and exitbutton2.isOver(pos):
+                gamescreen = False
+    if timetowait > 0:
+        pygame.time.wait(timetowait)
+        numberofcardsturned = 0
+        timetowait = 0
                 
-                if gamescreen == False and yellowbutton5x4.isOver(pos):
-                    gameDisplay.fill(black)
-                    gamescreen = True
-                    get_deck()
-                    for Card in deck:
-                        Card.draw(gameDisplay, 0)
-                    exitbutton2.draw(gameDisplay)
-
-                if gamescreen == False and exitbutton.isOver(pos):
-                    quit()
-            #MOUSE CLICK IN-GAME
-            else:
-                for i in range(0,len(deck)):
-                    print(len(deck))
-                    if deck[i].isOver(pos):
-                            deck[i].outline = 2
-                            deck[i].color == (0,200,100)
-                            deck_flipped.append(deck[i])
-                            print(deck[i])
-                            gameDisplay.fill(black)
-                            if len(deck_flipped) == 1:
-                                j=i
-                            deck_flipped[0].draw(gameDisplay, 0)
-                            for Card in deck:
-                                Card.draw(gameDisplay, 0)
-                            if len(deck_flipped) == 2:
-                                deck[j].outline = 0
-                                deck[i].outline = 0
-                                if deck[j].index == deck[i].index:
-                                    print("hello?")
-                                    deck.pop(i)
-                                    if i > j:
-                                        deck.pop(j+1)
-                                    else:
-                                        deck.pop(j-1)
-                                deck_flipped = []
-
-
-
-                if gamescreen == True and exitbutton2.isOver(pos):
-                    gameDisplay.fill(black)
-                    gamescreen = False
-
     pygame.display.flip()
 
-print(event)
-
-
-#pygame.display.update()
 clock.tick(60)
